2cd1da8fde82c1aef44a44b4ddd5b0b315d5a44d
Bogdan Boginskiy <bogdan_boginskiy@mail.ru>
2020-02-23 21:22:01 +0300

    Right way

--- lecture 1/TaskOne/src/TaskOne.java
+++ lecture 1/TaskOne/src/TaskOne.java
@@ -9,35 +9,39 @@ public class TaskOne {
      * @return an modified array
      */
     public static int[] minMax(int[] array) {
-        int max = array[array.length - 1];
-        int indexMax = array.length - 1;
-        int min = array[0];
-        int indexMin = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-                indexMin = i;
+        if (array.length > 1) {
+            int max = array[array.length - 1];
+            int indexMax = array.length - 1;
+            int min = array[0];
+            int indexMin = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] < min) {
+                    min = array[i];
+                    indexMin = i;
+                }
+                if (array[i] > max) {
+                    max = array[i];
+                    indexMax = i;
+                }
             }
-            if (array[i] > max) {
-                max = array[i];
-                indexMax = i;
+            if (array[0] != min) {
+                array[indexMin] = array[0];
+                array[0] = min;
             }
-        }
-        if (array[0] != min) {
-            array[indexMin] = array[0];
-            array[0] = min;
-        }
-        if (array[array.length - 1] != max) {
-            array[indexMax] = array[array.length - 1];
-            array[array.length - 1] = max;
+            if (array[array.length - 1] != max) {
+                array[indexMax] = array[array.length - 1];
+                array[array.length - 1] = max;
+            }
         }
         return array;
     }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(minMax(new int[] {5, 2, 6, 1, -2, 10, 4, 3, 7})));
+        System.out.println(Arrays.toString(minMax(new int[] {})));
     }
 }
     /* output:
     [-2, 2, 6, 1, 5, 7, 4, 3, 10]
+    []
     */
