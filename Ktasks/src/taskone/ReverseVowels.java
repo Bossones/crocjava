5eab0bd15125b9794fb7e1c588118b09dfd7d390
Bogdan Boginskiy <bogdan_boginskiy@mail.ru>
2020-03-19 17:25:45 +0300

    Ver. 1.0;

---
+++ Ktasks/src/taskone/ReverseVowels.java
@@ -0,0 +1,75 @@
+package taskone;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class ReverseVowels {
+
+    private String inputString;
+
+    private String outputString;
+
+    public ReverseVowels(String inputString) {
+        this.inputString = inputString;
+    }
+
+    public ReverseVowels() {
+        this("");
+    }
+
+    public String reverseVowelsInString(String inputString) {
+        this.inputString = inputString;
+        return reverseVowelsInString();
+    }
+
+    public String reverseVowelsInString() {
+        if (inputString.equals("") )
+            return outputString = "";
+
+        char[] outString = inputString.toCharArray();
+        boolean next = true;
+        char buffer;
+        for (int i = 0, y = outString.length - 1; i != y;) {
+            if (next) {
+                if (isVowel(outString[i])) {
+                    next = false;
+                } else i++;
+            }
+            if (!next) {
+                if (isVowel(outString[y])) {
+                    buffer = outString[i];
+                    outString[i] = outString[y];
+                    outString[y] = buffer;
+                    next = true;
+                    i++;
+                }
+                if (i != y)
+                    y--;
+            }
+        }
+        return outputString = String.copyValueOf(outString);
+    }
+
+    private boolean isVowel(char symbol) {
+        return symbol == 'a' || symbol == 'e' || symbol == 'o' || symbol == 'y' || symbol == 'i' || symbol == 'u';
+    }
+
+    public String getReversedString() {
+        return (outputString == null) ? "" : outputString;
+    }
+
+    public static void main(String[] args) {
+        ReverseVowels rev = new ReverseVowels("index");
+        ReverseVowels rev2 = new ReverseVowels("palindrome");
+        ReverseVowels rev3 = new ReverseVowels("garage");
+        ReverseVowels rev4 = new ReverseVowels("reflect");
+        ReverseVowels rev5 = new ReverseVowels("array");
+        System.out.println(rev.reverseVowelsInString());
+        System.out.println(rev2.reverseVowelsInString());
+        System.out.println(rev3.reverseVowelsInString());
+        System.out.println(rev4.reverseVowelsInString());
+        System.out.println(rev5.reverseVowelsInString());
+    }
+}
